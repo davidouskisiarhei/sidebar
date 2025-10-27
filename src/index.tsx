9685275905fe7553c849369c2dc8656d6808d373
Siarhei Davidouski <davidouskisiarhei@gmail.com>
2025-10-27 10:00:05 +0300

    подключена библиотека gh-pages и проведена настройка для деплоя проекта на github pages

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import App from "./App"
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={'/sidebar'}>
       <Routes>
         <Route index element={<App />} />
         <Route path={":menuId"}>
